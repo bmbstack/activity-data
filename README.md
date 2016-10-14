de56a68c090f60479b7b6f0c9fb9c68f46fa54f2
wangmingjob <wangmingjob@icloud.com>
2016-10-14 13:27:34 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# activity-data
+> susuan activity data
+
+## Development
+
+```shell
+npm i cooking -g
+npm run dev
+```
+
+## Production
+```
+npm run dist
+```
+
+## License
+MIT
---
+++ src/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>activity-data</h1>
+    <p>hello, this is a activity data page</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  };
+</script>
